1e469168b838a0394afa69eae9f7443bb341de1d
Vaishnavi0911 <vshelke9604@gmail.com>
2023-03-29 15:42:03 -0700

    Spring mvc-Path Variable Annotation

---
+++ springmaven-pathvariable_Annotation/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+
+	
+	
+	@RequestMapping(value = "/success/{username}/{password}")
+	public String login(@PathVariable("username")String username,@PathVariable("password")String password)
+	{
+		System.out.println("welcome");
+		System.out.println(username);
+		System.out.println(password);
+		return "success";
+	}
+}
